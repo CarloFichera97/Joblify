e8746dc4cc9cbe84efd7c2f5227dbbe34a852634
Carlo Fichera <Carlo.fichera97@gmail.com>
2021-06-24 10:51:36 +0100

    Added firebase Database URL

--- webpack.config.js
+++ webpack.config.js
@@ -24,6 +24,9 @@ module.exports = (env) => {
         "process.env.FIREBASE_API_KEY": JSON.stringify(
           process.env.FIREBASE_API_KEY
         ),
+        "process.env.FIREBASE_DATABASE_URL": JSON.stringify(
+          process.env.FIREBASE_DATABASE_URL
+        ),
         "process.env.FIREBASE_AUTH_DOMAIN": JSON.stringify(
           process.env.FIREBASE_AUTH_DOMAIN
         ),
